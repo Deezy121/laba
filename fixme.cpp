e77da424e60a7267c59d50821fdea52eaaf38e4f
Deezy121 <vovkosss@yandex.ru>
2022-06-03 00:58:45 +0300

    Измененное

--- fixme.cpp
+++ fixme.cpp
@@ -1,5 +1,5 @@
 
-qqqqqqqq#include "../settings/Settings.h"
+fffffff#include "../settings/Settings.h"
 #include "../db/DbConnection.h"
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
